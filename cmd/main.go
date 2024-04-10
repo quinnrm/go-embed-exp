0476bd2195ff76c259775e037f9417bf586f97b6
Ryan Quinn <130491640+quinnrm@users.noreply.github.com>
2024-04-10 17:27:07 -0500

    Setup up project to use a cmd folder.
    Added embedData internal package since all embed paths are relative to
    the package. For instance, moving `main.go` to `cmd/main.go` meant the embedded files
    would need to be in `cmd/`
    
    The answer to this was to create the `embedData` package, and import the
    files after moving them there.
    
    Changes to be committed:
    	modified:   cmd/main.go
    	new file:   internal/embedData/embedData.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+
+	"github.com/quinnrm/go-embed-exp/internal/embedData"
 )
 
 func main() {
 	fmt.Println("Go embed experiments.")
 
-	//efsData, err := embedData.EFS.ReadDir(".")
-	//if !errors.Is(err, nil) {
-	//	log.Fatalf("ERROR: (main) Problem reading embeded filesystem root dir, %v", err)
-	//}
+	efsData, err := embedData.EFS.ReadDir(".")
+	if !errors.Is(err, nil) {
+		log.Fatalf("ERROR: (main) Problem reading embeded filesystem root dir, %v", err)
+	}
 
-	//fmt.Println("Listing the files in the embedded filesystem.")
-	//for i, d := range efsData {
-	//	fmt.Printf("index; %d, is dir: %t, name: %s\n", i, d.IsDir(), d.Name())
-	//}
+	fmt.Println("Listing the files in the embedded filesystem.")
+	for i, d := range efsData {
+		fmt.Printf("index; %d, is dir: %t, name: %s\n", i, d.IsDir(), d.Name())
+	}
 
-	//fmt.Println()
-	//fmt.Println("Read \"hello.txt\" from the embeded FS.")
+	fmt.Println()
+	fmt.Println("Read \"hello.txt\" from the embeded FS.")
 
-	//txtHello, err := eFS.ReadFile("hello/hello.txt")
-	//if !errors.Is(err, nil) {
-	//	log.Fatalf("ERROR: (main) Problem with \"hello.txt\", %v", err)
-	//}
+	txtHello, err := embedData.EFS.ReadFile("hello/hello.txt")
+	if !errors.Is(err, nil) {
+		log.Fatalf("ERROR: (main) Problem with \"hello.txt\", %v", err)
+	}
 
-	//fmt.Println("Print \"hello.txt\"")
-	//fmt.Println(string(txtHello))
+	fmt.Println("Print \"hello.txt\"")
+	fmt.Printf("%s", string(txtHello))
 }
